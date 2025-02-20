846cac8fc6839a2866e3a828cb1e844fb1c9a955
최현준 <choijjunii1@gmail.com>
2025-02-20 12:52:29 +0900

    JSON 과정중 ERR_INVALID_ARG_TYPE

--- app.js
+++ app.js
@@ -1,38 +1,52 @@
 //* 서버 생성하기 //
-// http 리콰이어 불러오기 , url 불러오기
+// http 리콰이어 불러오기 , url 불러오기 ,fs
 const { error } = require('console');
 const http = require('http');
 const url = require('url');
+const fs = require('fs');
+const qs = require('querystring');
+const path = require('path');
 // createServer 활용, request respose 매개변수//
-const server = http.createServer(function (req, res) {
-  
-  //* GET 생성(홈,글 목록, 글상세) 404에러 설정.//
+// error404 설정( error.html을 만들어서 html로 가게 요청)
+const server = http.createServer(function (req, res, err) {
+  if (err) {
+    const errorFile = fs.readFileSync('error.html');
+    res.writeHead(404, { "Content-Type": "text/html" })
+    res.end(errorFile);
+  }
+  //* GET 생성(홈,글 목록, 글상세) //
   // method,url 변수처리삭제 , pathname 활용//
   const pathname = url.parse(req.url, true).pathname;
-  try {
-    if (req.method === 'GET') {
-      if (pathname === '/') {
-        res.writeHead(200);
-        res.end('메인 페이지');
-      } else if (pathname === '/list') {
-        res.writeHead(200)
-        res.end('글 목록')
-      } if (pathname === '/info') {
-        res.writeHead(200)
-        res.end('상세보기')
-      }
+  if (req.method === 'GET') {
+    if (pathname === '/') {
+      const home = fs.readFileSync('index.html');
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(home);
+    } else if (pathname === '/list') {
+      res.writeHead(200)
+      res.end('list')
+    } if (pathname === '/info') {
+      res.writeHead(200)
+      res.end('info')
     }
-  } catch(error) {
-    console.log(error);
-    res.statusCode(404, {"Content-Type": "application:json"})
-    res.end();
   }
-  //* POST 생성(글 작성 부분)//
-  if (pathname === '/add' && req.method === 'POST') {
-    
-  }
+
   
- 
+  //* POST 생성(글 작성 부분)//
+  //form data JSON 변환 과정 //
+  else if (pathname === '/add' && req.method === 'POST') {
+    let body = "";
+    req.on('data', (chunk) => {
+      body = body + chunk;
+      const filePath = path.join(__dirname, "data.json")
+      fs.writeFile(filePath, JSON.stringify(chunk));
+    })
+    req.on('end', () => {
+      const string = qs.parse(body);
+      console.log(string);
+      fs.appendFileSync('data.JSON', string);
+    })
+  }
 });
 
 //* 포트 8000설정, 콘솔로그 주소찍기//
