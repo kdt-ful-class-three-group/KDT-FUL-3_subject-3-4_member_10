0016b3751a1b03e7d3d397800dfab1f69da0bc7e
최현준 <choijjunii1@gmail.com>
2025-02-20 10:48:09 +0900

    PORT8000, 주소확인

--- app.js
+++ app.js
@@ -5,4 +5,11 @@ const http = require('http');
 // createServer 활용, request respose 매개변수//
 const server = http.createServer(function (req, res) {
   
+//todo GET 생성//
+
+});
+
+//* 포트 8000설정, 콘솔로그 주소찍기//
+server.listen(8000, function () {
+  console.log('서버진행중.. http://localhost:8000/');
 });
