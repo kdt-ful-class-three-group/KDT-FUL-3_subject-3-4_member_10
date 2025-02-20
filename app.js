9862133b883be3f7588a9eaca5c4b5853c41e37c
최현준 <choijjunii1@gmail.com>
2025-02-20 11:55:33 +0900

    err 404 처리과정

--- app.js
+++ app.js
@@ -1,32 +1,38 @@
 //* 서버 생성하기 //
-// http 리콰이어 불러오기 //
+// http 리콰이어 불러오기 , url 불러오기
+const { error } = require('console');
 const http = require('http');
-
+const url = require('url');
 // createServer 활용, request respose 매개변수//
 const server = http.createServer(function (req, res) {
   
-  //todo 경로 받아오는 작업 fs활용
-  //* GET 생성(홈,글 목록, 글상세)//
-  // method, url 변수//
-  const method = req.method;
-  const url = req.url;
-  if (method === 'GET') {
-    if (url === '/') {
-      
-    } else if (url === '/list') {
-      
-    } else if (url === '/info') {
-      
+  //* GET 생성(홈,글 목록, 글상세) 404에러 설정.//
+  // method,url 변수처리삭제 , pathname 활용//
+  const pathname = url.parse(req.url, true).pathname;
+  try {
+    if (req.method === 'GET') {
+      if (pathname === '/') {
+        res.writeHead(200);
+        res.end('메인 페이지');
+      } else if (pathname === '/list') {
+        res.writeHead(200)
+        res.end('글 목록')
+      } if (pathname === '/info') {
+        res.writeHead(200)
+        res.end('상세보기')
+      }
     }
-    //* POST 생성(글 작성 부분)//
-    //todo .on(data,end) 활용//
-  } else if (method === 'POST') {
-    if (url === '/add') {
-      
-    }
+  } catch(error) {
+    console.log(error);
+    res.statusCode(404, {"Content-Type": "application:json"})
+    res.end();
   }
-
-
+  //* POST 생성(글 작성 부분)//
+  if (pathname === '/add' && req.method === 'POST') {
+    
+  }
+  
+ 
 });
 
 //* 포트 8000설정, 콘솔로그 주소찍기//
