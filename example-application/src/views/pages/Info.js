ccb199ac48102e0725ed8f1d3482a0bc20711305
최현준 <choijjunii1@gmail.com>
2025-02-26 17:58:59 +0900

    대문자 ->소문자로 변경완료

--- example-application/src/views/pages/Info.js
+++ example-application/src/views/pages/Info.js
@@ -5,6 +5,7 @@ import { divTag } from "../forms/tag.js";
 //* 상세보기 페이지 
 //* 상세보기 페이지도 위의 리스트 페이지와 같이 콜백으로 동적 html 구현.
 //* new Date 메서드 활용. 
+
 // newDate만 사용했을경우 -> Mon Feb 24 2025 14:04:47 GMT+0900 (대한민국 표준시)
 // 처음에는 new Date().toISOString() 을 사용했는데 뒤에 '초'와 필요없는 영어가 나와서 LocaleString사용.
 function infoPage(index) {
